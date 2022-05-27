f5fea4f41363ba713740221105f87cd1fac5fa8e
antonio.martin <antonio.martin@schibsted.com>
2022-05-28 01:53:42 +0200

    24: improving credits

--- components/common/credits/index.d.ts
+++ components/common/credits/index.d.ts
@@ -1,11 +1,8 @@
 interface PeopleCredit {
   id: number;
-  known_for_department: string;
   name: string;
-  popularity: number;
   profile_path: string;
   character: string;
-  credit_id: string;
 }
 
 interface PeopleCredits {
